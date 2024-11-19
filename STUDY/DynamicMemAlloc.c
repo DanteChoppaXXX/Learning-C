6067d203de6f2e4b26f0bd88df2e5f32ce2e3a8e
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-11-19 08:58:06 +0000

    STUDY

---
+++ STUDY/DynamicMemAlloc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // void* malloc(size_t, size);
+
+    // Allocate Memory for an integer using malloc().
+    int *number = (int *)malloc(sizeof(int));
+    // Check for successful allocation.
+    if (number == NULL)
+    {
+        printf("Memory Allocation Failed!");
+        return 1; // Exit if malloc fails.
+    }
+
+    *number = 419; // Store an integer value at the allocated memory.
+
+    printf("Address of the memory block: %p\n", number);
+    printf("Value stored at allocated memory: %d\n", *number);
+
+    free(number); // Always free the allocated memory after use.
+
+    // Allocate Memory for an array using malloc().
+    int numberOfElements = 5;
+    int *array = (int *)malloc(numberOfElements * sizeof(int));
+    // Check for successful allocation.
+    if (array == NULL)
+    {
+        printf("Memory Allocation Failed!");
+        return 1; // Exit if malloc fails.
+    }
+
+    // Assign values to the array.
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        array[i] = i * 5;
+    }
+
+    // Display the elements of the array.
+
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+
+    free(array); // Always free the allocated memory after use.
+
+    return 0;
+}
