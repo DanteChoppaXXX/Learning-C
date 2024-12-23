e9b67b7ee06fcc095336202f96f4920708e424a2
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-12-23 01:47:35 +0000

    STUDY

--- STUDY/S_RDataOverNet/Server.c
+++ STUDY/S_RDataOverNet/Server.c
@@ -4,6 +4,8 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+#define PORT 8080 // Port number for the server
+
 int main()
 {
 
@@ -19,6 +21,26 @@ int main()
 
     printf("Socket created successfully! Socket descriptor: %d\n", server_socket);
 
+    // Define server address.
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;         // IPv4
+    address.sin_addr.s_addr = INADDR_ANY; // Listen on any available interface
+    address.sin_port = htons(PORT);       // Convert PORT to network byte order
+
+    // Bind the socket.
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("Bind Failed!\n");
+        close(server_socket); // Close the socket if binding fails
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Socket successfully bound to port %d!\n", PORT);
+
+    // Start listening
+    /* code */
+
+    
     // Close the socket.
     close(server_socket);
 
