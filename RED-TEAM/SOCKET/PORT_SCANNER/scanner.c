81c16632a1956c3d38afd59f69845f8e16225ff0
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-01-05 16:00:48 +0000

    PORT_SCANNER

--- RED-TEAM/SOCKET/PORT_SCANNER/scanner.c
+++ RED-TEAM/SOCKET/PORT_SCANNER/scanner.c
@@ -10,6 +10,7 @@ This program scans a specific range of ports on a target IP or hostname to deter
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <errno.h>
 #include <sys/time.h>
@@ -18,7 +19,7 @@ This program scans a specific range of ports on a target IP or hostname to deter
 // Program Variables.
 char targetIPOrHostname[100];
 int startPort, endPort;
-int timeout = 3;
+int timeout;
 
 enum Protocol_Type
 {
@@ -27,12 +28,80 @@ enum Protocol_Type
     BOTH = 3 // Work on this later; if(both){ create two separate threads to scan tcp and udp concurrently }
 };
 
+enum Port_Status
+{
+    OPEN = 1,
+    CLOSED = 2,
+    FILTERED = 3
+};
+
+// Function to resolve Hostname to IP Address.
+void *resolveHostname(char *hostname)
+{
+    struct hostent *host;
+    host = gethostbyname(hostname);
+    if (host == NULL)
+    {
+        printf("Error: %s\n", strerror(errno));
+        exit(1);
+    }
+    strcpy(targetIPOrHostname, inet_ntoa(*((struct in_addr *)host->h_addr_list[0])));
+}
+
 int main()
 {
     pthread_t thread1, thread2; // Threads for parallel scanning
     int thread1_result, thread2_result;
     // Get the target IP or hostname from the user
     printf("Enter the target IP or hostname: ");
+    scanf("%s", targetIPOrHostname);
+
+    // Check if the input is a hostname and resolve it to an IP address
+    if (inet_addr(targetIPOrHostname) == -1)
+    {
+        resolveHostname(targetIPOrHostname);
+    }
+
+    printf("Target IP: %s\n", targetIPOrHostname);
+
+    // Get the port range from the user
+    printf("Enter the start port: ");
+    scanf("%d", &startPort);
+    printf("Enter the end port: ");
+    scanf("%d", &endPort);
+
+    // Get the protocol type from the user
+    int protocolType;
+    printf("Enter the protocol type (1 for TCP, 2 for UDP, 3 for both): ");
+    scanf("%d", &protocolType);
+
+    // Validate the user input
+    if (protocolType < 1 || protocolType > 3)
+    {
+        printf("Invalid protocol type\n");
+        return 1;
+    }
+
+    char *protocol;
+
+    if (protocolType == 1)
+    {
+        protocol = "TCP";
+    }
+    else if (protocolType == 2)
+    {
+        protocol = "UDP";
+    }
+    else
+    {
+        protocol = "BOTH";
+    }
+
+    // Get the timeout value from the user
+    printf("Enter the timeout value (in seconds): ");
+    scanf("%d", &timeout);
+
+    printf("Scanning ports %d-%d on %s using %s protocol, with %d seconds timeout...\n", startPort, endPort, targetIPOrHostname, protocol, timeout);
 
     return 0;
 }
