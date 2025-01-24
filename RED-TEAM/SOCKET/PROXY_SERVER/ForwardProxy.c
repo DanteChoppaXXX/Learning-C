0b46e601e10ccea66114dbffec536213cacc6920
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-01-24 03:52:15 +0000

    PROXY_SERVER

--- RED-TEAM/SOCKET/PROXY_SERVER/ForwardProxy.c
+++ RED-TEAM/SOCKET/PROXY_SERVER/ForwardProxy.c
@@ -8,6 +8,8 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <sys/stat.h>
+#include <netdb.h>
+
 
 #define PROXY_PORT 8888
 #define BUFFER_SIZE 4096
@@ -19,6 +21,8 @@ typedef struct
 {
     int client_socket;
     int server_socket;
+    struct sockaddr_in client_address;
+    socklen_t client_address_len;
 } ProxyArgs;
 
 // Client handler function.
@@ -32,24 +36,106 @@ void *client_handler(void *args){
     {
         perror("Failed To Receive Request!");
         close(client_socket);
+        free(proxy_args);
+        return NULL;
     }
     else if (bytesReceived == 0)
     {
-        perror("Connection Close!");
+        perror("Connection Closed!");
         close(client_socket);
+        free(proxy_args);
+        return NULL;
     }
     else{
         buffer[bytesReceived] = '\0';
         printf("%s\n", buffer);
 
-        // Extract the target server and resource from the HTTP request
-        char method[10], path[50], http_version[10];
-        sscanf(buffer, "%s %s %s", method, path, http_version);
-        printf("Host: %s\nResource: %s\nMethod: %s\n", path, http_version, method);
+        // Extract the target host and resource URI from the HTTP request Header.
+
+        /* code */
+
+
+        // Forward the request to the target server and send the response back to the client.
+        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_socket < 0)
+        {
+            perror("Failed To Create Server Socket!");
+            close(client_socket);
+            free(proxy_args);
+            return NULL;
+        }
+
+        // Define the address structure for the target server.
+        struct sockaddr_in server_address;
+        socklen_t server_address_len = sizeof(server_address);
+
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(80);
+
+        struct hostent *host = gethostbyname(path);
+        if (host == NULL)
+        {
+            perror("Failed To Get Host By Name!");
+            close(client_socket);
+            close(server_socket);
+            free(proxy_args);
+            return NULL;
+        }
+
+        memcpy(&server_address.sin_addr, host->h_addr_list[0], host->h_length);
+
+        // Connect to the target server.
+        if (connect(server_socket, (struct sockaddr *)&server_address, server_address_len) < 0)
+        {
+            perror("Failed To Connect To Server!");
+            close(client_socket);
+            close(server_socket);
+            free(proxy_args);
+            return NULL;
+        }
+
+        // Forward the client request to the target server.
+        if (send(server_socket, buffer, bytesReceived, 0) < 0)
+        {
+            perror("Failed To Forward Request To Server!");
+            close(client_socket);
+            close(server_socket);
+            free(proxy_args);
+            return NULL;
+        }
+
+        // Receive the response from the target server.
+        ssize_t bytesSent;
+        while ((bytesSent = recv(server_socket, buffer, sizeof(buffer), 0)) > 0)
+        {
+            if (send(client_socket, buffer, bytesSent, 0) < 0)
+            {
+                perror("Failed To Forward Response To Client!");
+                close(client_socket);
+                close(server_socket);
+                free(proxy_args);
+                return NULL;
+            }
+        }
+
+        if (bytesSent < 0)
+        {
+            perror("Failed To Receive Response From Server!");
+            close(client_socket);
+            close(server_socket);
+            free(proxy_args);
+            return NULL;
+        }
+
+        close(server_socket);
+
+        printf("Request Forwarded Successfully!\n");
 
     }
-    
 
+    close(client_socket);
+    free(proxy_args);
+    return NULL;
 }
 
 // Main function.
@@ -57,7 +143,7 @@ int main()
 {
     // Create a TCP socket.
     int proxy_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(proxy_socket == NULL){
+    if(proxy_socket < 0){
         perror("Failed To Create Socket!");
         exit(1);
     }
@@ -94,30 +180,39 @@ int main()
 
     while (1)
     {
-        client_socket = accept(proxy_socket, (struct sockaddr *)&proxy_address, &proxy_address_len);
+        struct sockaddr_in client_address;
+        socklen_t client_address_len = sizeof(client_address);
+        client_socket = accept(proxy_socket, (struct sockaddr *)&client_address, &client_address_len);
+        if (client_socket < 0)
+        {
+            perror("Failed To Accept Connection!");
+            continue;
+        }
 
         // Allocate memory for thread arguments.
         ProxyArgs *proxy_args = malloc(sizeof(ProxyArgs));
         if (proxy_args == NULL)
         {
             perror("Memory Allocation Failed!");
-            close(proxy_socket);
-            exit(1);
+            close(client_socket);
+            continue;
         }
 
         proxy_args->client_socket = client_socket;
+        proxy_args->client_address = client_address;
+        proxy_args->client_address_len = client_address_len;
 
         // Create a new thread for each client.
         if(pthread_create(&clientThread, NULL, client_handler, (void *)proxy_args))
         {
             perror("Failed To Create Thread!");
+            close(client_socket);
             free(proxy_args);
-            exit(1);
+            continue;
         }
 
         pthread_detach(clientThread);
     }
-
 
     // Close the socket after handling the client
     close(proxy_socket);
